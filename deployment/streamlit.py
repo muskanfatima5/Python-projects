328010e38fe6744f93a396dedc081f35500720fe
muskanfatima5 <160497988+muskanfatima5@users.noreply.github.com>
2025-04-05 14:21:02 +0500

    Delete deployment directory

--- deployment/streamlit.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-st.title("Simple Streamlit App")
-
-user_input = st.text_input("Enter some text:")
-
-if st.button("Show Text"):
-    st.write(f"You entered: {user_input}")
